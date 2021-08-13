fbebe783434d027abeb2e23de32aca09003b0d76
Zensynthium <47935832+Zensynthium@users.noreply.github.com>
2021-08-13 14:58:25 -0400

    added route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/digiocean', (req, res) => {
+  res.send('Hello World from Digital Ocean!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
